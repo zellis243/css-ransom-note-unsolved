0c478b07dbb8cd0cc15650d325ad6fb0feaedcde
Andrey Codez <andreycodez@bixtrix.com>
2024-09-13 21:52:40 +0500

    Updated Readme.md. Added Live server dependency

--- README.md
+++ README.md
@@ -1,32 +1,49 @@
 # The Ransom Note Assignment
 
+The CSS text styling assignment.
+
 ## Introduction
 
 Welcome to the intriguing world of typography and design with CSS! In this assignment, we challenge you to unleash your creative genius by crafting a captivating ransom note with unique styles for each letter. Your task is to bring harmony to chaos, ensuring every character shines with its individuality. Embrace the power of typography and let your imagination run wild as you transform a simple note into a mesmerizing visual masterpiece. Get ready to captivate the eye and impress us with your distinctive approach to letter styling.
 
 Best of luck on this typographic adventure!
 
+To preview the app, simply click the "Go Live" button found in the VSCODE status bar. This will open your webpage in your browser.
+
+If you are not using VSCode you can install and run the "live-server" npm package following the list of commands:
+
+- Use `npm i live-server` to install the live-server package
+- Use `npm run live-server` or `npx live-server --port=5551` to run the live-server. The port number should be 5551 strictly.
+
+The live-server installation video: Video
+The live-server NPM package page: Link
+
 ## Learning objectives
 
 This assignment should prove that a student is able to:
 
-- Use the color CSS property to set the font color
+- Use the color CSS property to set the color of the text
 - Change the text (font) styles using CSS rules to specify:
   - the size of the characters
-  - the weight of the font
-  - any text transformations
-  - font decorations
+  - parameters of the font
+  - text transformations
+  - text decorations
 - Include and apply a custom font or a collection of fonts from Google Fonts
 
 ## Standard requirements
 
-- [ ] Download the zipped project file, and extract it
-- [ ] Move the extracted folder into the project's storage and open the project in VSCode using the terminal
+- [ ] Fork the project to your github account
+- [ ] Clone the project to your computer
+- [ ] Open the project in VSCode with `code <cloned_repo_folder_name>`
+- [ ] **This Assignment is NOT tested. You can use alll the desired creativity!**
 - [ ] All the tasks of the "Specific requirements" section MUST be solved
 - [ ] **The project's file/folder structure should NOT be edited!**
+- [ ] **The index.html file should NOT be edited!**
 - [ ] VSCode IDE MUST have 0 problems listed
+- [ ] The code of the index.html and styles.css should pass the W3C validation (Feel free to use either the "[w3c web validator](https://marketplace.visualstudio.com/items?itemName=CelianRiboulet.webvalidator)" VSCode extension or the "[Online w3c Markup Validation Service](https://validator.w3.org/#validate_by_input)")
 - [ ] The code MUST be formatted with Prettier.
-- [ ] Compress (zip) the project folder and submit it through the assignment form, when finished.
+- [ ] Push the changes to the Github repo, when finished.
+- [ ] Submit a .txt file with the Github repo url.
 
 ## Specific requirements
 
@@ -39,7 +56,7 @@ This assignment should prove that a student is able to:
   - Font transformation
   - Font family
 - [ ] Set the font size using fixed pixel values.
-- [ ] Use two color systems of your choice for setting the color (hex, rgb, hsl, etc.).
+- [ ] Use at least two color systems of your choice for setting the color (hex, rgb, hsl, etc.).
 - [ ] Ensure each font weight value from the given list is used at least once:
   - Bold
   - Normal
@@ -54,4 +71,4 @@ This assignment should prove that a student is able to:
 - [ ] Style every letter in the message.
 - [ ] Group selectors with the same styles.
 - [ ] The HTML document should NOT have inline styles.
-- [ ] Find and connect 3 more random fonts from Google fonts. Feel free to use [this article](https://www.webdesignerdepot.com/2021/09/21-exceptional-google-fonts-you-probably-havent-discovered-yet/) for the fonts inspiration.
+- [ ] There are 3 Google Fonts connected to the project. Find and connect 3 more random fonts from Google fonts. Feel free to use [this article](https://www.webdesignerdepot.com/2021/09/21-exceptional-google-fonts-you-probably-havent-discovered-yet/) or the [Google Fonts](https://fonts.google.com/) platform for the fonts inspiration.
--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-const spanList = [...document.getElementsByTagName('span')];
+const spanList = [...document.getElementsByTagName("span")];
 
 const getRandomNumber = (max) => {
   return Math.floor(Math.random() * max);
@@ -23,127 +23,127 @@ const fontSizeArr = [
   48, 42, 48, 45, 44,
 ];
 
-const fontsArr = ['Piazzolla', 'Ceviche One', 'Raleway Dots'];
+const fontsArr = ["Piazzolla", "Ceviche One", "Raleway Dots"];
 
-const textDecorationArr = ['underline', 'line-through', 'overline', 'none'];
-const textTransformArr = ['uppercase', 'lowercase'];
+const textDecorationArr = ["underline", "line-through", "overline", "none"];
+const textTransformArr = ["uppercase", "lowercase"];
 
 const letterColors = [
-  'f40c3e',
-  '582446',
-  'ead305',
-  '89ad47',
-  '983f37',
-  '963b52',
-  '645892',
-  '77c502',
-  'c52ab0',
-  '671d97',
-  '315410',
-  '166d5c',
-  '8460ec',
-  '3b405f',
-  'bbd78f',
-  'cfa1a7',
-  '4ef2ca',
-  '828bbf',
-  '708ba5',
-  '7b6b9a',
-  'f573e7',
-  '4c986b',
-  '825775',
-  '2d8f',
-  'bcabe4',
-  '50b73d',
-  '6315c4',
-  '4834cc',
-  'e932d8',
-  '793e47',
-  'b99491',
-  '24e187',
-  '6453d7',
-  '19506a',
-  'ff0de8',
-  'fc8d13',
-  'd30c22',
-  'c6d107',
-  'c33fdc',
-  'c21bd8',
-  'd7dcec',
-  '3668b6',
-  '704452',
-  'cc3ae6',
-  '141561',
-  '2f8339',
-  '9a44e4',
-  '222a73',
-  '6c3645',
-  'ceffc6',
-  '5d964d',
-  '34f215',
-  'e33e24',
-  '6ce7e1',
-  '5cbe9',
-  '18016c',
-  'b53b18',
-  'ecc181',
-  '966f31',
-  '11e854',
-  '5dd993',
-  'b65f67',
-  '44b135',
-  'e4ed4b',
-  'f0dd09',
-  'da7dc2',
-  '2b389d',
-  'c28275',
-  '6565e0',
-  'd1fb3',
-  'baac6',
-  'f5b00a',
-  'b7abe9',
-  'bab48d',
-  'c4c754',
-  '2d9a2e',
-  '350113',
-  '88c3d0',
-  'c78a0c',
-  'ef055a',
-  'a288aa',
-  '14bee4',
-  'c2d33c',
-  '22a4d9',
-  'f2fc7f',
-  '6941a9',
-  'e18478',
-  '107a69',
-  'e6cf62',
-  '96f27f',
-  '75fa97',
-  '6b684a',
-  '54b9a6',
-  '4d74c0',
-  'eb2c1a',
-  '2d68cc',
-  '9e8dae',
-  'dac4d',
-  '8edc17',
-  '41b465',
+  "f40c3e",
+  "582446",
+  "ead305",
+  "89ad47",
+  "983f37",
+  "963b52",
+  "645892",
+  "77c502",
+  "c52ab0",
+  "671d97",
+  "315410",
+  "166d5c",
+  "8460ec",
+  "3b405f",
+  "bbd78f",
+  "cfa1a7",
+  "4ef2ca",
+  "828bbf",
+  "708ba5",
+  "7b6b9a",
+  "f573e7",
+  "4c986b",
+  "825775",
+  "2d8f",
+  "bcabe4",
+  "50b73d",
+  "6315c4",
+  "4834cc",
+  "e932d8",
+  "793e47",
+  "b99491",
+  "24e187",
+  "6453d7",
+  "19506a",
+  "ff0de8",
+  "fc8d13",
+  "d30c22",
+  "c6d107",
+  "c33fdc",
+  "c21bd8",
+  "d7dcec",
+  "3668b6",
+  "704452",
+  "cc3ae6",
+  "141561",
+  "2f8339",
+  "9a44e4",
+  "222a73",
+  "6c3645",
+  "ceffc6",
+  "5d964d",
+  "34f215",
+  "e33e24",
+  "6ce7e1",
+  "5cbe9",
+  "18016c",
+  "b53b18",
+  "ecc181",
+  "966f31",
+  "11e854",
+  "5dd993",
+  "b65f67",
+  "44b135",
+  "e4ed4b",
+  "f0dd09",
+  "da7dc2",
+  "2b389d",
+  "c28275",
+  "6565e0",
+  "d1fb3",
+  "baac6",
+  "f5b00a",
+  "b7abe9",
+  "bab48d",
+  "c4c754",
+  "2d9a2e",
+  "350113",
+  "88c3d0",
+  "c78a0c",
+  "ef055a",
+  "a288aa",
+  "14bee4",
+  "c2d33c",
+  "22a4d9",
+  "f2fc7f",
+  "6941a9",
+  "e18478",
+  "107a69",
+  "e6cf62",
+  "96f27f",
+  "75fa97",
+  "6b684a",
+  "54b9a6",
+  "4d74c0",
+  "eb2c1a",
+  "2d68cc",
+  "9e8dae",
+  "dac4d",
+  "8edc17",
+  "41b465",
 ];
 
-document.addEventListener('DOMContentLoaded', () => {
-  const spanList = Array.from(document.getElementsByTagName('span'));
+document.addEventListener("DOMContentLoaded", () => {
+  const spanList = Array.from(document.getElementsByTagName("span"));
   Array.from(spanList).forEach((element, index) => {
     const turnDirection = getRandomNumber(2) === 0 ? -1 : 1;
     const degValue = Math.floor(Math.random() * 12) * turnDirection;
     const skewArr = getRandomSkewArr();
     const positionOffset = 20;
-    const isBold = getRandomNumber(8) % 2 === 0 ? 'bold' : 'normal';
+    const isBold = getRandomNumber(8) % 2 === 0 ? "bold" : "normal";
     const decoration =
       textDecorationArr[getRandomNumber(textDecorationArr.length)];
     const transform =
       textTransformArr[getRandomNumber(textTransformArr.length)];
-    const borderToPosition = getRandomNumber(2) === 0 ? 'top' : 'bottom';
+    const borderToPosition = getRandomNumber(2) === 0 ? "top" : "bottom";
     const positionOffsetValue = getRandomNumber(positionOffset);
     const fontFamily = fontsArr[getRandomNumber(fontsArr.length)];
 
@@ -164,11 +164,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // element.style.backgroundColor = `#${getRandomColor()}`;
 
-    element.addEventListener('mouseover', (e) => {
+    element.addEventListener("mouseover", (e) => {
       e.target.style.transform = `rotate(${degValue}deg) skew(${skewArr[0]}deg, ${skewArr[1]}deg) scale(1.2)`;
     });
 
-    element.addEventListener('mouseleave', (e) => {
+    element.addEventListener("mouseleave", (e) => {
       e.target.style.transform = `rotate(${degValue}deg) skew(${skewArr[0]}deg, ${skewArr[1]}deg) scale(1)`;
     });
   });
